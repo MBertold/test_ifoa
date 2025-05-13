8bf195b8ffc2121670db7808ae500869819394ad
MBertold <marco_bertoldi@hotmail.it>
2025-05-13 16:53:21 +0200

    zz

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 import requests
+import pandas as pd
 
 load_dotenv()
-API_KEY = os.getenv('api_key')
+#API_KEY = os.getenv('api_key')
 
 def main():
     st.title("Open Weather API")
@@ -13,15 +14,25 @@ def main():
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}'
     result = requests.get(url)
     json = result.json()
+    temperatura =json['main']["temp"] - 273.15
+    temperatura = round(temperatura,2)
+    vento = json["wind"]["speed"]
+    umidità = json['main']["humidity"]
+    pressione = json['main']["pressure"]
+    lat = json["coord"]["lat"]
+    lon = json["coord"]["lon"]
+    
     if st.button("Cerca"):
         a, b = st.columns(2)
         c, d = st.columns(2)
 
-        a.metric("Temperature", json['main']["temp"],None , border=True)
-        b.metric("Wind", "4 mph", None, border=True)
+        a.metric("Temperature",f"{temperatura} °C" ,delta=None)
+        b.metric("Wind",f"{vento} m/sec" , delta=None)
 
-        c.metric("Humidity", json['main']["humidity"], None, border=True)
-        d.metric("Pressure", json['main']["pressure"], None, border=True)
+        c.metric("Humidity",f"{umidità} %" ,delta= None)
+        d.metric("Pressure",f"{pressione} hPa" ,delta= None)
+        
+        st.map(pd.DataFrame({'lat':[lat],'lon':[lon]},columns=['lat','lon']))
 
 if __name__ == "__main__":
     main()
